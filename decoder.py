ce01f982fe2afc9d8eb288c14f3e62834f4bbf07
Y.O.L.A <37326068+Ultraicee@users.noreply.github.com>
2022-06-27 10:22:58 +0800

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -10,18 +10,17 @@ import tensorflow as tf
 
 def decoder_forward(feature_in, tps_weight, sz_params):
     """
-    Do matrix multiplication between the TPS matrix and the control points
+    Do matrix multiplication between the A matrix of tps and the control points
     to solve disparity of TPS interpolation area.
-    @param feature_in:
-    @param tps_weight:
-    @param sz_params:
-    @return: disp_ex:
+    @param feature_in: control points
+    @param tps_weight: A matrix of tps
+    @return: disp_ex: padded disparity
     """
 
     # Before interpolation, crop the upper and
     # right edges of the image. The range of cut is determined by image situation.
     disp_vec = tf.map_fn(lambda x: tf.matmul(tps_weight, x), feature_in)  # d = Ax
-    d_height,d_width= get_tps_size(sz_params) # size of cropped image, expect to be 200x200
+    d_height,d_width= get_tps_size(sz_params) # size of cropped image
     disp_tensor = tf.reshape(disp_vec, [tf.shape(feature_in)[0], d_height, d_width, 1])  # shape = [b h w 1]
     paddings = tf.constant([[0,0],[sz_params.crop_top,sz_params.crop_bottom],[sz_params.crop_left,sz_params.crop_right],[0,0]]) # padding by zeros
     disp_ex = tf.pad(disp_tensor, paddings, "CONSTANT")
@@ -43,11 +42,7 @@ def decoder_forward2(feature_in, feature_in_base, tps_weight, disp_base, sz_para
 
 def get_tps_size(sz_params):
     """
-    sz_params = size_params(batch=50,
-                    height=288,
-                    width=360,
-                    channel=3,
-                    crop_pos)
+    get A matrix shape by crop params and width, height 
     """
     # Get the height and width of the TPS interpolation area
     tps_height = np.int32(sz_params.input_height - sz_params.crop_top - sz_params.crop_bottom)
